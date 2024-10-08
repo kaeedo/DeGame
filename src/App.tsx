5e5f90e546539076035ca96a97c6354ba0292aa1
Kai Ito <kai.ito@valora.com>
2024-10-08 17:17:06 +0200

    Initial rot-js test

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,15 @@
-import {SolidLeafletMap} from "solidjs-leaflet"
-import geojson from './assets/laender.json?raw'
-import stations from './assets/stations.json?raw';
+import { createSignal, onCleanup } from "solid-js";
 
-function App() {
-  const a = JSON.parse(geojson)
+export function App() {
+  const [count, setCount] = createSignal<number>(0);
 
-  var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
+  const interval = setInterval(() => setCount((c) => c + 1), 1000);
 
-const b = JSON.parse(stations)
+  onCleanup(() => clearInterval(interval));
 
   return (
     <>
-      <div>
-      <SolidLeafletMap
-      center={[51.165691, 10.451526]}
-      id="map"
-      zoom={6}
-      onMapReady={(l, m) => {
-        l.geoJSON(a).addTo(m);
-        l.geoJSON(b, {pointToLayer:(feature:any,latLng:any) => l.circleMarker(latLng, geojsonMarkerOptions)}).addTo(m);
-      }}
-    />
-      </div>
+      <div class="text-slate-950">Count: {count()}</div>
     </>
-  )
+  );
 }
-
-export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,30 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import { App } from "./App";
 
-const root = document.getElementById('root')
+import { Display, RNG } from "rot-js";
+import { DisplayOptions } from "rot-js/lib/display/types";
 
-render(() => <App />, root!)
+const o: Partial<DisplayOptions> = {
+  width: 80,
+  height: 50,
+  layout: "hex",
+};
+
+const display = new Display(o);
+const canvas = display.getContainer()!;
+
+document.body.append(canvas);
+
+for (var y = 0; y < 5; y++) {
+  for (var x = y % 2; x < 8; x += 2) {
+    var bg = RNG.getItem(["#333", "#666", "#999", "#ccc", "#fff"]);
+    display.draw(x, y, "•", "#000", bg);
+  }
+}
+
+const root = document.getElementById("root");
+
+render(() => <App />, root!);
